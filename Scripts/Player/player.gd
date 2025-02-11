4d8d6d2850d803ba9d58c7d725d77c360a75ca49
ShinySkink9185 <shinyskink9185@gmail.com>
2025-02-10 20:24:59 -0500

    Klonoa Fixes and Coding
    Trying to get animations to work mostly.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 90.000
 const JUMP_VELOCITY = -240.0
 
+@onready var sprite = $Sprite2D
+@onready var animation = $AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,5 +23,29 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Handle direction facing.
+	if direction > 0:
+		sprite.flip_h = false
+	else: if direction < 0:
+		sprite.flip_h = true
+	
+	# TODO: Figure out how to make animations loop for falling
+	# Animations!
+	if is_on_floor():
+		if direction == 0:
+			animation.play("Stand")
+		else:
+			animation.play("Walk")
+	else: if velocity.y < 0:
+		animation.play("Jump")
+	else:
+		animation.play("Fall")
+	
 	move_and_slide()
+
+# Animation loops
+func _on_animation_player_animation_finished(animation):
+	if animation.get_current_animation() == "Fall":
+		animation.play("Fall")
+		animation.seek(0.2667)
